969bbbe0e4cd4b11b58c8aa33aabb21d85e072a0
Martin Maeland <31969005+martinmaeland@users.noreply.github.com>
2021-03-27 18:56:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='dev',
+    maintainer='martinmaeland',
     maintainer_email='martinmaeland@outlook.com',
-    description='TODO: Package description',
+    description='ROS2 node for data transfer between ground station to drone.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
